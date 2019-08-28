87442f01841d1ff658717665b326a4cefb3f8475
franklinmelo <franklinmelo18@gmail.com>
2019-08-28 14:23:22 -0300

    feat: implement progressBar in activity_login

--- ThreeJS-Kotlin/app/src/main/java/com/example/threejskotlin/RenderActivity.kt
+++ ThreeJS-Kotlin/app/src/main/java/com/example/threejskotlin/RenderActivity.kt
@@ -8,7 +8,6 @@ import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
-import com.example.threejskotlin.R
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
